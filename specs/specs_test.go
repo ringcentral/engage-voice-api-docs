8e8f1f102acd88a990a606a9225fdbd53e932cb3
John Wang <johncwang@gmail.com>
2020-05-14 09:11:08 -0700

    update Engage Voice OAS3 spec/tests

--- specs/specs_test.go
+++ specs/specs_test.go
@@ -10,7 +10,7 @@ var specTests = []struct {
 	filepath string
 	title    string
 }{
-	{"specs/engage-voice_openapi3.json", "RingCentral Engage Voice API"},
+	{"engage-voice_openapi3.json", "RingCentral Engage Voice API"},
 }
 
 // TestSpecs test reading specs.
