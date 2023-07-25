7e9e259f51a99ec03f797e60cbd4b7bc6b2cd5de
da kong <kongda9999@gmail.com>
2023-07-25 13:35:06 +0800

    fixed js sample code; fixed look-like-being-missed dial-group sample code

--- code-samples/active-calls/quick-start.js
+++ code-samples/active-calls/quick-start.js
@@ -6,13 +6,13 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 const RunRequest = async function () {
     // Instantiate the SDK wrapper object with your RingCentral app credentials
     const ev = new EngageVoice({
-	'clientId':     process.env.RC_CLIENT_ID,
-	'clientSecret': process.env.RC_CLIENT_SECRET
+        clientId: process.env.RC_CLIENT_ID,
+        clientSecret: process.env.RC_CLIENT_SECRET
     })
 
     try {
         // Authorize with your RingCentral Office user credentials
-        await ev.authorize({ process.env.RC_JWT })
+        await ev.authorize({ jwt: process.env.RC_JWT })
 
         // Get Active Calls
         const endpoint = "/api/v1/admin/accounts/{accountId}/activeCalls/list?product=ACCOUNT&productId={accountId}"
--- code-samples/dialing/quick-start.js
+++ code-samples/dialing/quick-start.js
@@ -8,13 +8,12 @@ const RunRequest = async function () {
 
     // Instantiate the SDK wrapper object with your RingCentral app credentials
     const ev = new EngageVoice({
-	'clientId':     process.env.RC_CLIENT_ID,
-	'clientSecret': process.env.RC_CLIENT_SECRET
+        clientId: process.env.RC_CLIENT_ID,
+        clientSecret: process.env.RC_CLIENT_SECRET
     })
-
     try {
         // Authorize with your RingCentral Office user credentials
-        await ev.authorize({ process.env.RC_JWT })
+        await ev.authorize({ jwt: process.env.RC_JWT })
 
         // Create a new Dial Group
         const postBody = {
